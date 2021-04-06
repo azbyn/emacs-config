eb5defbb3ec10ed231ecc98f4660bd5981f4ac20
azbyn <andreypavel1@yahoo.com>
2021-04-06 19:51:37 +0300

    Removed custom-set-variables

--- init.el
+++ init.el
@@ -27,20 +27,19 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+
+;; don't create the package-selected-packages variable
+(defun package--save-selected-packages (&optional value)
+  "Set and save `package-selected-packages' to VALUE."
+  (when value
+    (setq package-selected-packages value)))
+
+
 ;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 (when (file-readable-p "~/.emacs.d/keybindings.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/keybindings.org")))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(vdiff clang-format all-the-icons org-html-themes lsp org-wiki julia-mode fsharp-mode haskell-interactive-mode inf-haskell company-auctex auctex lsp-csharp csharp-mode php-mode ob-ipython wucuo clang-format+ irony-eldoc platformio-mode company-arduino arduino-mode lsp-java dap-mode lsp-ui lsp-mode flycheck-java emms mpdel dad-joke haskell-mode fish-completion dumb-jump jedi help-fns+ elpy company-jedi hl-todo nasm-mode company-racer rust-mode zop-to-char anzu htmlize rmsbolt magit smartparens nlinum nlinum-mode smex xterm-color multiple-cursors persp-mode smart-comment ranger fish-mode highlight-escape-sequences highlight-numbers avy lua-mode window-numbering company-dcd find-file-in-project diminish company dashboard expand-region rainbow-mode ivy org-bullets popup-kill-ring rainbow-delimiters which-key use-package base16-theme))
- )
 
 (defconst azbyn/windows-mode (string-equal system-type "windows-nt"))
 (defvar azbyn/height (cond
