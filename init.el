464c176ad651011244e9454be8cf536b4bbff666
azbyn <andreypavel1@yahoo.com>
2021-04-06 12:37:29 +0300

    Added diff-region.el

--- init.el
+++ init.el
@@ -59,7 +59,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#1d1f21" :foreground "#c5c8c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "ADBO" :family "DejaVu Sans Mono"))))
+ `(default ((t (:inherit nil :stipple nil :background "#1d1f21" :foreground "#c5c8c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height ,azbyn/height :width normal :foundry "ADBO" :family ,azbyn/font))))
  '(flyspell-duplicate ((t (:underline (:color "#12A59C" :style wave)))))
  '(flyspell-incorrect ((t (:underline (:color "#12A59C" :style wave)))))
  '(font-lock-comment-delimiter-face ((t (:foreground "#969896"))))
